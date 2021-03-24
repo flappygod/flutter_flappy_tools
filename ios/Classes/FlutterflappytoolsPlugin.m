4c2085600e768845a6fc9a37000dda3425e3910e
lijunlin <327603258@qq.com>
2021-03-24 18:16:21 +0800

    222

--- ios/Classes/FlutterflappytoolsPlugin.m
+++ ios/Classes/FlutterflappytoolsPlugin.m
@@ -245,11 +245,7 @@
             // 判断手机是否安装百度地图
             if ([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"baidumap://"]]) {
                 
-                NSString *urlString = [[NSString stringWithFormat:@"baidumap://map/direction?origin={{我的位置}}&destination=name:%@|latlng:%f,%f|addr:%@&mode=driving&coord_type=gcj02",
-                                        title,
-                                        [lat floatValue],
-                                        [lng floatValue],
-                                        title] stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+                NSString *urlString = [[NSString stringWithFormat:@"baidumap://map/direction?origin={{我的位置}}&destination=latlng:%f,%f|name=%@&mode=driving&coord_type=gcj02",[lat floatValue],[lng floatValue],title] stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
                 [[UIApplication sharedApplication] openURL:[NSURL URLWithString:urlString]];
                 
                 result(@"1");
